e7eef2540a9db24bf22427c65e30b896cc62e818
Yujun(Xavier) Lin <16437040+synxlin@users.noreply.github.com>
2020-06-24 18:40:14 -0700

    change check API from AT_CHECK to TORCH_CHECK for Pytorch 1.5
    Signed-off-by: Xavier Lin <syn.xavier.lin+git@gmail.com>

--- modules/functional/src/utils.hpp
+++ modules/functional/src/utils.hpp
@@ -4,17 +4,17 @@
 #include <ATen/cuda/CUDAContext.h>
 #include <torch/extension.h>
 
-#define CHECK_CUDA(x) AT_CHECK(x.type().is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
 
 #define CHECK_CONTIGUOUS(x)                                                    \
-  AT_CHECK(x.is_contiguous(), #x " must be a contiguous tensor")
+  TORCH_CHECK(x.is_contiguous(), #x " must be a contiguous tensor")
 
 #define CHECK_IS_INT(x)                                                        \
-  AT_CHECK(x.scalar_type() == at::ScalarType::Int,                             \
+  TORCH_CHECK(x.scalar_type() == at::ScalarType::Int,                             \
            #x " must be an int tensor")
 
 #define CHECK_IS_FLOAT(x)                                                      \
-  AT_CHECK(x.scalar_type() == at::ScalarType::Float,                           \
+  TORCH_CHECK(x.scalar_type() == at::ScalarType::Float,                           \
            #x " must be a float tensor")
 
 #endif
